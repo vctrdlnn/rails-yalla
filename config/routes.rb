88a10fcb745d23e80c412d0ed270ef4bd5d96031
Victor Delannoy <victor.delannoy@gmail.com>
2016-11-22 12:38:55 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /en|fr|de/ do
-    devise_for :users
     root to: 'pages#home'
+    devise_for :users do
+      get 'profile'
+    end
+
+    resources :trips do
+      resources :activities
+      resources :trip_days, only: [:create, :destroy, :update, :show]
+      member do
+        put 'like'
+        get 'print'
+      end
+    end
+
+    resources :activities do
+      member do
+        put 'pin'
+      end
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
