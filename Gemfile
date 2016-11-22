e8079ad5893a4ec03c30077956a9824af3a0b26e
Victor Delannoy <victor.delannoy@gmail.com>
2016-11-22 10:12:05 +0100

    added all gems and set up figaro

--- Gemfile
+++ Gemfile
@@ -20,12 +20,31 @@ gem 'autoprefixer-rails'
 group :development, :test do
   gem 'binding_of_caller'
   gem 'better_errors'
-  
+
   gem 'pry-byebug'
   gem 'pry-rails'
   gem 'spring'
   gem 'listen', '~> 3.0.5'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem "letter_opener" # mails
 end
 
+# add ones:
+gem "rails-controller-testing"
+gem 'rails_12factor', group: :production
+gem 'cloudinary', '1.1.0'
+gem 'carrierwave'
+gem 'devise'
+gem 'omniauth-facebook'
 
+gem 'simple-form-datepicker'
+gem 'momentjs-rails', '>= 2.9.0'
+gem 'bootstrap3-datetimepicker-rails', '~> 4.14.30'
+
+gem "geocoder"
+gem "coffee-rails"
+gem "gmaps4rails"
+
+source 'https://rails-assets.org' do
+  gem "rails-assets-underscore"
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  # saves the location before loading each page so we can return to the
+  # right page. If we're on a devise page, we don't want to store that as the
+  # place to return to (for example, we don't want to return to the sign in page
+  # after signing in), which is what the :unless prevents
+  before_filter :store_current_location, :unless => :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :mobile])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :mobile])
+  end
+
+  # override the devise helper to store the current location so we can
+  # redirect to it after loggin in or out. This override makes signing in
+  # and signing up work automatically.
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
 end
