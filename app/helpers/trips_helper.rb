5db6f365e2bdd9ea2fd5c9f3c27a01d3cfc5dc1c
Victor Delannoy <victor.delannoy@gmail.com>
2016-12-01 11:54:39 +0100

    photo

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -14,7 +14,7 @@ module TripsHelper
 
   def trip_photo_index_path(trip)
     if trip.photo_url.nil?
-      cl_image_path("city2_glimf7.jpg", width: 400, height: 300, crop: :fill)
+      cl_image_path("watch_window_default.jpg", :height=>300, :width=>500, :crop=>"fill")
     else
       cl_image_path(trip.photo, width: 400, height: 300, crop: :fill)
     end
@@ -22,9 +22,18 @@ module TripsHelper
 
   def trip_photo_index_tag(trip)
     if trip.photo_url.nil?
-      cl_image_tag("city2_glimf7.jpg", width: 400, height: 300, crop: :fill)
+      cl_image_tag("watch_window_default.jpg", :height=>300, :width=>500, :crop=>"fill")
     else
       cl_image_tag(trip.photo, width: 400, height: 300, crop: :fill)
     end
   end
+
+  def trip_photo_show_path(trip)
+    if trip.photo_url.nil?
+      cl_image_path("watch_window_default.jpg", :height=>350, :width=>1400, :crop=>"fill")
+    else
+      cl_image_path(trip.photo, width: 1400, height: 600, crop: :fill)
+    end
+  end
+
 end
