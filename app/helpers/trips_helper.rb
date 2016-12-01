2aa0c70b85e0e778264b91b2374c1e4731a33381
Victor Delannoy <victor.delannoy@gmail.com>
2016-12-01 11:43:15 +0100

    solving picture and trip days bugs

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -11,4 +11,20 @@ module TripsHelper
       "liked_trips"
     end
   end
+
+  def trip_photo_index_path(trip)
+    if trip.photo_url.nil?
+      cl_image_path("city2_glimf7.jpg", width: 400, height: 300, crop: :fill)
+    else
+      cl_image_path(trip.photo, width: 400, height: 300, crop: :fill)
+    end
+  end
+
+  def trip_photo_index_tag(trip)
+    if trip.photo_url.nil?
+      cl_image_tag("city2_glimf7.jpg", width: 400, height: 300, crop: :fill)
+    else
+      cl_image_tag(trip.photo, width: 400, height: 300, crop: :fill)
+    end
+  end
 end
